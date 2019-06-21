8ea805eed5a5515970f4a3a9f738c4641d7f7861
giovannidiguardo <giovannidiguardo@mbp-di-giovanni.lan>
2019-06-21 22:40:50 +0200

    completato punto 2

--- src/it/gdgsoft/tdp/libretto/Libretto.java
+++ src/it/gdgsoft/tdp/libretto/Libretto.java
@@ -19,4 +19,15 @@ public class Libretto {
 	public void add(Voto v) {
 		voti.add(v);
 	}
+	
+	public List<Voto> cercaVoti(int punti) {
+		List<Voto> result = new ArrayList<Voto>();
+		
+		for (Voto v : this.voti) {
+			if (v.getPunti()==punti) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
 }
--- src/it/gdgsoft/tdp/libretto/TestLibretto.java
+++ src/it/gdgsoft/tdp/libretto/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.gdgsoft.tdp.libretto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -18,6 +19,9 @@ public class TestLibretto {
 		libr.add(new Voto(24, "Complementi di Economia", LocalDate.of(2018, 2, 15)));
 		libr.add(new Voto(25, "Logistica", LocalDate.of(2019, 2, 1)));
 		libr.add(new Voto(27, "Programmazione a Oggetti", LocalDate.of(2017, 1, 25)));
+		
+		List<Voto> venticinque = libr.cercaVoti(25);
+		System.out.println(venticinque);
 	}
 
 }
