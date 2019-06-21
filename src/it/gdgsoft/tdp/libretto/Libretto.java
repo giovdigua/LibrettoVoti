71c1228db3332db71e7ffda7b66502ea7671ea9a
giovannidiguardo <giovannidiguardo@mbp-di-giovanni.lan>
2019-06-21 22:18:54 +0200

    Implementato il punto 1

--- src/it/gdgsoft/tdp/libretto/Libretto.java
+++ src/it/gdgsoft/tdp/libretto/Libretto.java
@@ -1,5 +1,22 @@
 package it.gdgsoft.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
+	private List<Voto> voti;
+	
+	public Libretto() {
+		
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	/**
+	 * Aggiungi un nuovo voto al libretto
+	 * @param v il {@link Voto} da aggiungere
+	 */
+	public void add(Voto v) {
+		voti.add(v);
+	}
 }
